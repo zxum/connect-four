b0865b48629976925b0bf4175f9a3a28705cb763
zxum <zion.um@outlook.com>
2020-09-09 14:10:05 -0500

    Added in pseudocode as comments.

---
+++ lib/main.rb
@@ -0,0 +1,15 @@
+# Pseudocode 
+# Start Game 
+# Show instructions 
+# Players pick colors of tokens
+# Start round 1 
+# See board 
+# Player 1 picks column 
+# Display updated board 
+# Check if game is over 
+# Player 2 picks column 
+# Check if game is over 
+# repeat rounds until game_over 
+# game is over when all cells are filled or winner is true
+# winner is true if Player 1 or Player 2 tokens make a line of 4 
+# Display winner 
