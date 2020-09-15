7722ac908515a09e835fbfd6fa2126326097f7e7
zxum <zion.um@outlook.com>
2020-09-15 16:35:45 -0500

    create Board class.

--- lib/main.rb
+++ lib/main.rb
@@ -1,15 +1,42 @@
-# Pseudocode 
-# Start Game 
-# Show instructions 
+# Pseudocode
+# Start Game (initialize board and players)
+# Show instructions
 # Players pick colors of tokens
-# Start round 1 
-# See board 
-# Player 1 picks column 
-# Display updated board 
-# Check if game is over 
-# Player 2 picks column 
-# Check if game is over 
-# repeat rounds until game_over 
+# Start round 1
+# See board
+# Player 1 picks column
+# Display updated board
+# Check if game is over
+# Player 2 picks column
+# Check if game is over
+# repeat rounds until game_over
 # game is over when all cells are filled or winner is true
-# winner is true if Player 1 or Player 2 tokens make a line of 4 
-# Display winner 
+# winner is true if Player 1 or Player 2 tokens make a line of 4
+# Display winner
+
+class Board 
+  attr_accessor :positions 
+
+  def initialize
+    @positions = make_board 
+  end
+
+  def display_board
+    puts "╔════════════════╗"
+    puts "  1 2 3 4 5 6 7  "
+    @positions.each do |row|
+      puts "  #{row.join(" ")} "
+    end 
+    puts "╚════════════════╝"
+  end
+
+  private 
+  def make_board 
+    b = []
+    6.times { b.push(%w(◌)*7) }
+    b
+  end 
+end
+
+myBoard = Board.new 
+
