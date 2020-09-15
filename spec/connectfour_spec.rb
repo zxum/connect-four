a8d234bbf740abf055a4058fa81dfb80a615fda8
zxum <zion.um@outlook.com>
2020-09-15 16:44:02 -0500

    Create tests for Game. Checks when board is full, and when there is a win.

--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -33,4 +33,116 @@ describe 'Connect Four game logic' do
   end
 
 
+
+  describe "The Game" do 
+
+    it "game over when board full" do 
+      game1 = Game.new 
+      game1.board.positions = [
+        ["◎", "◎", "●", "◎", "◎", "●", "●"],
+        ["●", "◎", "◎", "●", "●", "◎", "◎"],
+        ["◎", "●", "◎", "◎", "◎", "●", "●"],
+        ["●", "◎", "●", "●", "●", "◎", "◎"],
+        ["◎", "●", "●", "◎", "◎", "●", "●"],        
+        ["●", "◎", "●", "●", "◎", "◎", "●"]
+      ]
+      expect(game1.game_over?).to eq true
+    end
+    
+    it "board is full when no empty circle" do 
+      game1 = Game.new 
+      game1.board.positions = [
+        ["◎", "◎", "●", "◎", "◎", "●", "●"],
+        ["●", "◎", "◎", "●", "●", "◎", "◎"],
+        ["◎", "●", "◎", "◎", "◎", "●", "●"],
+        ["●", "◎", "●", "●", "●", "◎", "◎"],
+        ["◎", "●", "●", "◎", "◎", "●", "●"],        
+        ["●", "◎", "●", "●", "◎", "◎", "●"]
+      ]
+      expect(game1.board_full?).to eq true
+    end    
+    
+    
+    it "board is not full there are empty spots" do 
+      game1 = Game.new 
+      game1.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],        
+        ["●", "●", "●", "●", "◌", "◌", "◌"]
+      ]
+      expect(game1.board_full?).to eq false
+    end
+  
+    it "game over when winner" do 
+      game2 = Game.new 
+      game2.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],        
+        ["●", "●", "●", "●", "◌", "◌", "◌"]
+      ]
+      expect(game2.game_over?).to eq true
+    end
+
+    it "wins if four in a row" do 
+      game3 = Game.new 
+      game3.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "●", "●", "●"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],        
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"]
+      ]
+      expect(game3.row_win?).to eq true
+    end
+
+    it "wins if four in a col" do 
+      game4 = Game.new
+      game4.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"],        
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"]
+      ]
+      expect(game4.col_win?).to eq true
+    end 
+
+    it "wins if four in a forward diagonal" do
+      game5 = Game.new 
+      game5.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "●", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "●", "◌"],        
+        ["◌", "◌", "◌", "◌", "◌", "◌", "●"]
+      ]
+      expect(game5.fordia_win?).to eq true
+    end
+
+    it "wins if four in a backward diagonal" do 
+      game6 = Game.new
+      game6.board.positions = [
+        ["◌", "◌", "◌", "◌", "◌", "●", "◌"],
+        ["◌", "◌", "◌", "◌", "●", "◌", "◌"],
+        ["◌", "◌", "◌", "●", "◌", "◌", "◌"],
+        ["◌", "◌", "●", "◌", "◌", "◌", "◌"],
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"],        
+        ["◌", "◌", "◌", "◌", "◌", "◌", "◌"]
+      ]
+      expect(game6.backdia_win?).to eq true
+    end
+
+    
+
+  end
+
 end
